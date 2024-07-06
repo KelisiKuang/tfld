3f55ebe9e7fd92a53889245c3ced9b9ec9fbdca9
KelisiKuang <132179527+KelisiKuang@users.noreply.github.com>
2024-07-06 17:10:14 +0800

    Add files via upload

---
+++ wd1/translate.js
@@ -0,0 +1,447 @@
+var langs = {
+hr: {
+ title: "Imate li potrebu za kreditom",
+ step1: "Imate li potrebu za kreditom",
+ step2: "Da",
+}
+,
+ nl: {
+ title: "Heeft u behoefte aan een lening",
+ step1: "Heeft u behoefte aan een lening",
+ step2: "Ja",
+}
+,
+ bg: {
+ title: "Имате (1) ново съобщение",
+ step1: "Имате (1) ново съобщение",
+ step2: "Прочетете сега",
+}
+,
+ fil: {
+ title: "Mayroon kang (1) bagong mensahe",
+ step1: "Mayroon kang (1) bagong mensahe",
+ step2: "Basahin na ngayon",
+}
+,
+ de: {
+ title: "Möchten Sie einen Kredit von mindestens 300 Euro erhalten?",
+ step1: "Möchten Sie einen Kredit von mindestens 300 Euro erhalten?",
+ step2: "Ja",
+}
+,
+ id: {
+ title: "Anda memiliki (1) pesan baru",
+ step1: "Anda memiliki (1) pesan baru",
+ step2: "Baca sekarang",
+}
+,
+ ko: {
+ title: "(1)개의 새 메시지가 있습니다.",
+ step1: "(1)개의 새 메시지가 있습니다.",
+ step2: "지금 읽어라",
+}
+,
+ no: {
+ title: "Du har (1) ny melding",
+ step1: "Du har (1) ny melding",
+ step2: "Les Nå",
+}
+,
+ ms: {
+ title: "Anda mempunyai (1) mesej baharu",
+ step1: "Anda mempunyai (1) mesej baharu",
+ step2: "Baca Sekarang",
+}
+,
+ sv: {
+ title: "Du har (1) nytt meddelande",
+ step1: "Du har (1) nytt meddelande",
+ step2: "Läs nu",
+}
+,
+ sk: {
+ title: "Máte (1) novú správu",
+ step1: "Máte (1) novú správu",
+ step2: "Prečítajte si teraz",
+}
+,
+ ro: {
+ title: "Ai (1) mesaj nou",
+ step1: "Ai (1) mesaj nou",
+ step2: "Citeste acum",
+}
+,
+ ur: {
+ title: "آپ کے پاس (1) نیا پیغام ہے۔",
+ step1: "آپ کے پاس (1) نیا پیغام ہے۔",
+ step2: "ابھی پڑھیں",
+}
+,
+ te: {
+ title: "మీకు (1) కొత్త సందేశం ఉంది",
+ step1: "మీకు (1) కొత్త సందేశం ఉంది",
+ step2: "ఇప్పుడు చదవండి",
+}
+,
+ uk: {
+ title: "У вас є (1) нове повідомлення",
+ step1: "У вас є (1) нове повідомлення",
+ step2: "Читайте зараз",
+}
+,
+ th: {
+ title: "คุณมี (1) ข้อความใหม่",
+ step1: "คุณมี (1) ข้อความใหม่",
+ step2: "อ่านตอนนี้",
+}
+,
+ tg: {
+ title: "Шумо (1) паёми нав доред",
+ step1: "Шумо (1) паёми нав доред",
+ step2: "Ҳоло хонед",
+}
+,
+ sl: {
+ title: "Imate (1) novo sporočilo",
+ step1: "Imate (1) novo sporočilo",
+ step2: "Preberi zdaj",
+}
+,
+ ru: {
+ title: "У вас есть (1) новое сообщение",
+ step1: "У вас есть (1) новое сообщение",
+ step2: "Прочитай сейчас",
+}
+,
+ pl: {
+ title: "Masz (1) nową wiadomość",
+ step1: "Masz (1) nową wiadomość",
+ step2: "Czytaj teraz",
+}
+,
+ ml: {
+ title: "നിങ്ങൾക്ക് (1) പുതിയ സന്ദേശമുണ്ട്",
+ step1: "നിങ്ങൾക്ക് (1) പുതിയ സന്ദേശമുണ്ട്",
+ step2: "ഇപ്പോൾ വായിക്കുക",
+}
+,
+ it: {
+ title: "Hai (1) nuovo messaggio",
+ step1: "Hai (1) nuovo messaggio",
+ step2: "Leggi ora",
+}
+,
+ el: {
+ title: "Έχετε (1) νέο μήνυμα",
+ step1: "Έχετε (1) νέο μήνυμα",
+ step2: "Διάβασε τώρα",
+}
+,
+ lv: {
+ title: "Jums ir (1) jauns ziņojums",
+ step1: "Jums ir (1) jauns ziņojums",
+ step2: "Lasiet tūlīt",
+}
+,
+ fi: {
+ title: "Sinulla on (1) uusi viesti",
+ step1: "Sinulla on (1) uusi viesti",
+ step2: "Lue nyt",
+}
+,
+ cs: {
+ title: "Máte (1) novou zprávu",
+ step1: "Máte (1) novou zprávu",
+ step2: "Přečtěte si nyní",
+}
+,
+ ca: {
+ title: "Tens (1) missatge nou",
+ step1: "Tens (1) missatge nou",
+ step2: "Llegeix ara",
+}
+,
+ ar: {
+ title: "لديك (1) رسالة جديدة",
+ step1: "لديك (1) رسالة جديدة",
+ step2: "اقرا الان",
+}
+,
+ da: {
+ title: "Du har (1) ny besked",
+ step1: "Du har (1) ny besked",
+ step2: "Læs nu",
+}
+,
+ bn: {
+ title: "আপনি (1) নতুন বার্তা আছে",
+ step1: "আপনি (1) নতুন বার্তা আছে",
+ step2: "এখন পড়ুন",
+}
+,
+ et: {
+ title: "Teil on (1) uus sõnum",
+ step1: "Teil on (1) uus sõnum",
+ step2: "Loe kohe",
+}
+,
+ iw: {
+ title: "יש לך (1) הודעה חדשה",
+ step1: "יש לך (1) הודעה חדשה",
+ step2: "קרא עכשיו",
+}
+,
+ fr: {
+ title: "Souhaitez-vous obtenir un prêt d'au moins 300 euros?",
+ step1: "Souhaitez-vous obtenir un prêt d'au moins 300 euros?",
+ step2: "Oui",
+}
+,
+ lt: {
+ title: "Turite (1) naują pranešimą",
+ step1: "Turite (1) naują pranešimą",
+ step2: "Skaityti dabar",
+}
+,
+ mr: {
+ title: "तुमच्याकडे (1) नवीन संदेश आहे",
+ step1: "तुमच्याकडे (1) नवीन संदेश आहे",
+ step2: "आता वाचा",
+}
+,
+ ja: {
+ title: "(1) 件の新しいメッセージがあります",
+ step1: "(1) 件の新しいメッセージがあります",
+ step2: "今すぐ読む",
+}
+,
+ sr: {
+ title: "Имате (1) нову поруку",
+ step1: "Имате (1) нову поруку",
+ step2: "Прочитајте сада",
+}
+,
+ es: {
+ title: "¿Quiere obtener un préstamo de al menos 300 euros?",
+ step1: "¿Quiere obtener un préstamo de al menos 300 euros?",
+ step2: "Sí",
+}
+,
+ pt: {
+ title: "Você tem (1) nova mensagem",
+ step1: "Você tem (1) nova mensagem",
+ step2: "Leia agora",
+}
+,
+ vi: {
+ title: "Bạn có (1) tin nhắn mới",
+ step1: "Bạn có (1) tin nhắn mới",
+ step2: "Đọc ngay",
+}
+,
+ ta: {
+ title: "உங்களிடம் (1) புதிய செய்தி உள்ளது",
+ step1: "உங்களிடம் (1) புதிய செய்தி உள்ளது",
+ step2: "இப்போது படியுங்கள்",
+}
+,
+ tr: {
+ title: "(1) yeni mesajınız var",
+ step1: "(1) yeni mesajınız var",
+ step2: "Şimdi Oku",
+} ,
+ af: {
+ title: "Jy het (1) nuwe boodskap",
+ step1: "Jy het (1) nuwe boodskap",
+ step2: "Lees nou",
+}
+,
+ be: {
+ title: "У вас ёсць (1) новае паведамленне",
+ step1: "У вас ёсць (1) новае паведамленне",
+ step2: "Чытайце зараз",
+}
+,
+ hy: {
+ title: "Դուք ունեք (1) նոր հաղորդագրություն",
+ step1: "Դուք ունեք (1) նոր հաղորդագրություն",
+ step2: "Կարդացեք հիմա",
+}
+,
+ hu: {
+ title: "(1) új üzenete van",
+ step1: "(1) új üzenete van",
+ step2: "Olvasd Most",
+}
+,
+ kk: {
+ title: "Сізде (1) жаңа хабар бар",
+ step1: "Сізде (1) жаңа хабар бар",
+ step2: "Қазір оқыңыз",
+}
+,
+ ky: {
+ title: "Сизде (1) жаңы билдирүү бар",
+ step1: "Сизде (1) жаңы билдирүү бар",
+ step2: "Азыр оку",
+}
+,
+ fa: {
+ title: "شما (1) پیام جدید دارید",
+ step1: "شما (1) پیام جدید دارید",
+ step2: "الان بخوان",
+}
+,
+ my: {
+ title: "သင့်တွင် မက်ဆေ့ခ်ျအသစ် (၁) ခု ရှိသည်။",
+ step1: "သင့်တွင် မက်ဆေ့ခ်ျအသစ် (၁) ခု ရှိသည်။",
+ step2: "ယခုဖတ်ပါ။",
+}
+,
+ st: {
+ title: "U na le (1) molaetsa o mocha",
+ step1: "U na le (1) molaetsa o mocha",
+ step2: "Bala Joale",
+}
+,
+ zu: {
+ title: "Unomlayezo omusha (1).",
+ step1: "Unomlayezo omusha (1).",
+ step2: "Funda Manje",
+}
+,
+ si: {
+ title: "ඔබට නව පණිවිඩයක් (1) ඇත",
+ step1: "ඔබට නව පණිවිඩයක් (1) ඇත",
+ step2: "දැන් කියවන්න",
+}
+,
+ sw: {
+ title: "Una (1) ujumbe mpya",
+ step1: "Una (1) ujumbe mpya",
+ step2: "Soma Sasa",
+}
+,
+ uz: {
+ title: "Sizda (1) yangi xabar bor",
+ step1: "Sizda (1) yangi xabar bor",
+ step2: "Hozir oʻqing",
+}
+,
+ ne: {
+ title: "तपाईंसँग (1) नयाँ सन्देश छ",
+ step1: "तपाईंसँग (1) नयाँ सन्देश छ",
+ step2: "अब पढ्नुहोस्",
+}
+,
+ so: {
+ title: "Waxaad haysaa (1) fariin cusub",
+ step1: "Waxaad haysaa (1) fariin cusub",
+ step2: "Akhri hadda",
+}
+,
+ km: {
+ title: "អ្នកមាន (1) សារថ្មី។",
+ step1: "អ្នកមាន (1) សារថ្មី។",
+ step2: "អានឥឡូវនេះ",
+}
+,
+ hi: {
+ title: "आपके लिए एक नया संदेश है",
+ step1: "आपके लिए एक नया संदेश है",
+ step2: "अभी पढ़ो",
+}
+,
+ ht: {
+ title: "Ou gen (1) nouvo mesaj",
+ step1: "Ou gen (1) nouvo mesaj",
+ step2: "Li kounye a",
+}
+,
+ bs: {
+ title: "Imate (1) novu poruku",
+ step1: "Imate (1) novu poruku",
+ step2: "Pročitajte sada",
+}
+,
+ az: {
+ title: "(1) yeni mesajınız var",
+ step1: "(1) yeni mesajınız var",
+ step2: "İndi Oxu",
+}
+,
+ sq: {
+ title: "Ju keni (1) mesazh të ri",
+ step1: "Ju keni (1) mesazh të ri",
+ step2: "Lexojeni Tani",
+}
+,
+ ka: {
+ title: "თქვენ გაქვთ (1) ახალი შეტყობინება",
+ step1: "თქვენ გაქვთ (1) ახალი შეტყობინება",
+ step2: "წაიკითხეთ ახლა",
+}
+,
+ mg: {
+ title: "Manana (1) hafatra vaovao ianao",
+ step1: "Manana (1) hafatra vaovao ianao",
+ step2: "Vakio izao",
+}
+,
+ am: {
+ title: "(1) አዲስ መልእክት አለዎት",
+ step1: "(1) አዲስ መልእክት አለዎት",
+ step2: "አሁን አንብብ",
+}
+,
+ mn: {
+ title: "Танд (1) шинэ зурвас байна",
+ step1: "Танд (1) шинэ зурвас байна",
+ step2: "Одоо унш",
+}
+,
+ ps: {
+ title: "تاسو (1) نوی پیغام لرئ",
+ step1: "تاسو (1) نوی پیغام لرئ",
+ step2: "اوس ولولئ",
+}
+,
+ sd: {
+ title: "توھان وٽ آھي (1) نئون پيغام",
+ step1: "توھان وٽ آھي (1) نئون پيغام",
+ step2: "هاڻي پڙهو",
+}
+,
+ su: {
+ title: "Anjeun gaduh (1) pesen anyar",
+ step1: "Anjeun gaduh (1) pesen anyar",
+ step2: "Baca Ayeuna",
+} ,
+ en: {
+ title: "Do you want to get a loan of at least €300?",
+ step1: "Do you want to get a loan of at least €300?",
+ step2: "Yes",
+ }
+ ,
+};
+function getURLParameter(name) { 
+return decodeURI( 
+(RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1] || '' 
+); 
+}
+(function(d, n){
+    var langBrowser = n.language || (n.languages && n.languages[0]),
+        param = langs[langBrowser];
+    if (param == undefined && langBrowser && langBrowser.length > 0) {
+      var parsedLang = langBrowser.slice(0, 2);
+      if (langs[parsedLang]) {
+        param = langs[parsedLang];
+      }
+    }
+    if (param == undefined) {
+      param = langs["en"];
+    }
+    document.querySelector('title').innerHTML = param.title;
+    d.querySelector('#step1').innerHTML = param.step1;
+    d.querySelector('#step2').innerHTML = param.step2;
+})(document, navigator);
